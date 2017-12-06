30504e55b4769573ce40b32ccba2283a89e69bc0
王雨 <@wy-PC>
2017-12-06 20:15:37 +0800

    e3-manager-web-2

---
+++ e3-manager-web/src/main/java/cn/e3/manager/controller/ItemController.java
@@ -0,0 +1,34 @@
+package cn.e3.manager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3.manager.service.ItemService;
+import cn.e3.pojo.TbItem;
+import cn.e3.utils.DatagridPagebean;
+
+@Controller
+public class ItemController {
+	
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("item/list/{id}")
+	@ResponseBody
+	public TbItem findById(@PathVariable Long id){
+		TbItem item = itemService.findById(id);
+		return item;
+	}
+	
+	@RequestMapping("item/list")
+	@ResponseBody
+	public DatagridPagebean findItemListByPage(@RequestParam(defaultValue="1")Integer page, @RequestParam(defaultValue="30")Integer rows) {
+		DatagridPagebean pagebean = itemService.findItemListByPage(page, rows);
+		return pagebean;
+	}
+
+}
