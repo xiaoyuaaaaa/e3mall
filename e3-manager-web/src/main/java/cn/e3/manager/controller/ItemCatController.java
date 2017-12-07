22ee8125d54b1a58968249edd06f0b4bb09c3492
王雨 <@wy-PC>
2017-12-07 12:05:11 +0800

    e3-manager-web

---
+++ e3-manager-web/src/main/java/cn/e3/manager/controller/ItemCatController.java
@@ -0,0 +1,27 @@
+package cn.e3.manager.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3.manager.service.ItemCatService;
+import cn.e3.utils.TreeNode;
+
+@Controller
+public class ItemCatController {
+	
+	@Autowired
+	private ItemCatService itemCatService;
+	
+	@RequestMapping("/item/cat/list")
+	@ResponseBody
+	private List<TreeNode> findItemCatTreeNodeList(@RequestParam(value="id",defaultValue="0") Long parentId){
+		List<TreeNode> nodeList = itemCatService.findItemCatTreeNodeList(parentId);
+		return nodeList;
+	}
+
+}
